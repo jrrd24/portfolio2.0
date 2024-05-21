2e24f94bfcc9b90bee30305d01f99df1c1089c11
jrrd24 <jerardpagalilauan@gmail.com>
2024-05-22 00:10:55 +0800

    :construction: fixed project container colors

--- app/components/ProjectContainer.tsx
+++ app/components/ProjectContainer.tsx
@@ -6,24 +6,30 @@ type Props = {};
 const ProjectContainer = (props: Props) => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className=" w-[60rem] h-fit bg-slate-700 py-16 rounded-3xl relative">
-        <div className=" font-bold text-9xl absolute right-[-2.6rem] top-[-4rem] tracking-tightest">
+      <div className=" w-[56rem] h-fit bg-slate-100/70 shadow-2xl shadow-slate-500/30 dark:shadow-slate-700/30 dark:bg-slate-800/70 py-20 rounded-3xl relative">
+        {/**Number */}
+        <div
+          className=" font-bold text-9xl absolute right-[-2.6rem] top-[-4rem] tracking-tightest
+          bg-gradient-to-bl  from-custom-dark-light/40 dark:from-custom-white-dark/40  to-custom-dark-light/5 
+        dark:to-custom-white-dark/5 to-70% bg-clip-text text-transparent"
+        >
           01
         </div>
-        <div className=" grid grid-cols-4 gap-0 justify-items-center items-center ">
+        <div className=" grid grid-cols-4 gap-0 justify-items-center items-center z-10">
           {/**Image */}
-          <div className=" bg-red-700 w-72 h-96 rounded-2xl col-span-2">
-            Image here
-          </div>
+          <div className=" bg-slate-500 w-72 h-96 rounded-2xl col-span-2"></div>
           {/**Content */}
           <div className="col-span-2 pr-16">
-            <h1 className=" font-bold text-5xl ">ScaleUp Solutions</h1>
-            <h3 className=" font-regular text-base mt-4 text-custom-white-dark/80">
+            {/**Title */}
+            <h1 className=" font-extrabold text-5xl ">ScaleUp Solutions</h1>
+            {/**Description */}
+            <h3 className=" font-regular text-base mt-4 text-custom-dark-light/80 dark:text-custom-white-dark/80">
               The company website of ScaleUp Solutions Inc, A software services
               company that provides business solutions that scale up SME
               digitalization to improve quality of services
             </h3>
 
+            {/**Tags */}
             <div className="mt-6 flex flex-wrap gap-2">
               <Tag name="ASP.net" />
               <Tag name="HTML" />
--- app/components/Tag.tsx
+++ app/components/Tag.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Tag = (props: Props) => {
   return (
-    <div className="px-3 bg-slate-400 min-w-fit text-sm font-semibold rounded-lg h-8 flex justify-center items-center">
+    <div className="px-3 text-custom-dark/60 dark:text-custom-white/60 bg-slate-300 dark:bg-slate-700 min-w-fit text-sm font-semibold rounded-lg h-8 flex justify-center items-center">
       {props.name}
     </div>
   );
