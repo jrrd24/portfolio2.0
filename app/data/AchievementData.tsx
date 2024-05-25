0d891d36dacc3572297fc7a8afaa5953460da077
jrrd24 <jerardpagalilauan@gmail.com>
2024-05-25 17:38:29 +0800

    :speech_balloon: added achivements content

--- app/data/AchievementData.tsx
+++ app/data/AchievementData.tsx
@@ -2,6 +2,9 @@ const AchivementData = [
   {
     id: 1,
     title: "Power Up! Software Championing Filipino Workers Hackathon",
+    date: "April 2024",
+    order: 1,
+    image: "/assets/achievements/Hackathon.jpg",
     tags: [
       {
         id: 1,
@@ -14,9 +17,136 @@ const AchivementData = [
         order: 2,
       },
     ],
-    date: "April 2024",
-    order: 1,
-    image: "/assets/achievements/Hackathon.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Computer System Servicing NC II",
+    date: "December 2023",
+    order: 2,
+    image: "/assets/achievements/NC2.png",
+    tags: [
+      {
+        id: 1,
+        description: "🏅 Passer",
+        order: 1,
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    title: "International Conference on Information Technology Education ",
+    date: "November 2023",
+    order: 3,
+    image: "/assets/achievements/ICITE.png",
+    tags: [
+      {
+        id: 1,
+        description: "🏆 Qualifier",
+        order: 1,
+      },
+      {
+        id: 2,
+        description: "💬 White Paper Presentor",
+        order: 2,
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    title: "GDSC Info Session - UI/UX Design Using Figma",
+    date: "October 2023",
+    order: 4,
+    image: "/assets/achievements/GDSC_Speaker.png",
+    tags: [
+      {
+        id: 1,
+        description: "💬 Key Speaker",
+        order: 1,
+      },
+    ],
+  },
+
+  {
+    id: 5,
+    title: "International Conference on Information Technology Education ",
+    date: "November 2023",
+    order: 5,
+    image: "/assets/achievements/ICITE.png",
+    tags: [
+      {
+        id: 1,
+        description: "🏆 Top 25 Finalist",
+        order: 1,
+      },
+      {
+        id: 2,
+        description: "🏅 Most Promising SDG Solutions Award",
+        order: 2,
+      },
+    ],
+  },
+
+  {
+    id: 6,
+    title: "Philippine Civil Service Examination - Professional Level",
+    date: "August 2023",
+    order: 6,
+    image: "/assets/achievements/CSE.png",
+    tags: [
+      {
+        id: 1,
+        description: "🏅 Passer",
+        order: 1,
+      },
+    ],
+  },
+
+  {
+    id: 7,
+    title: "DICT Philippines Startup 7 - Pitching Competition",
+    date: "October 2022",
+    order: 7,
+    image: "/assets/achievements/DICT_Pitch.png",
+    tags: [
+      {
+        id: 1,
+        description: "🏆 Regional Finalist",
+        order: 1,
+      },
+    ],
+  },
+
+  {
+    id: 8,
+    title: "Google Developer Students Club - USLT",
+    date: "2020 - 2024",
+    order: 8,
+    image: "/assets/achievements/GDSC_Officer.png",
+    tags: [
+      {
+        id: 1,
+        description: "🛡️ Officer",
+        order: 1,
+      },
+    ],
+  },
+
+  {
+    id: 9,
+    title: "President's Lister",
+    date: "2022 - 2023",
+    order: 9,
+    image: "/assets/achievements/PL.png",
+    tags: [
+      {
+        id: 1,
+        description: "🏆 Awardee",
+        order: 1,
+      },
+    ],
   },
 ];
 
