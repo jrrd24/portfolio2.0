e54c63395b9afc8bf4ac2fbcc12ef67073738a9a
jrrd24 <jerardpagalilauan@gmail.com>
2024-06-05 17:24:34 +0800

    :speech_balloon: added contact content

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = (props: Props) => {
     <div className=" bg-transparent h-20 fixed z-50 w-full flex justify-center ">
       <div className="container mt-8 px-8 flex justify-between items-center">
         <a
-          href="/"
+          href="/#home"
           className=" text-xl lg:text-3xl bg-gradient-to-tr from-custom-orange via-custom-red to-custom-purple bg-clip-text text-transparent"
         >
           jp
--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -1,69 +1,55 @@
 import React from "react";
 import SectionNameImage from "../components/SectionNameImage";
+import { FaFacebook } from "react-icons/fa6";
+import ContactData from "../data/ContactData";
 
 type Props = {};
 
 const Contact = (props: Props) => {
   return (
     <section className="min-h-screen container relative">
-      {/* Bottom Circle */}
+      {/* Middle Circle */}
+      <div
+        className="rounded-full bg-radial-circle from-custom-orange/30 dark:from-custom-orange/40 from-0% via-custom-orange/20 dark:via-custom-orange/30 via-25% to-transparent to-70% 
+       h-[20rem] w-[12rem] md:h-[32rem] md:w-[36rem] blur-3xl absolute top-8 lg:top-0 right-24 lg:right-[16rem] z-0"
+      />
 
+      {/* Top Circle */}
       <div
         className="rounded-full bg-radial-circle from-custom-red/30 dark:from-custom-red/40 from-0% via-custom-red/20 dark:via-custom-red/30 via-25% to-transparent to-70% 
-       h-[36rem] w-[16rem] md:h-[32rem] md:w-[36rem] blur-3xl absolute top-[-8rem] lg:top-[0rem] right-[10rem] lg:right-[16rem] z-0"
+       h-[32rem] w-[32rem] md:h-[64rem] md:w-[48rem] blur-3xl absolute top-[-16rem] lg:top-[-32rem] right-[-12rem] lg:right-[0rem] z-0 "
       />
 
+      {/* Bottom Circle */}
       <div
-        className="rounded-full bg-radial-circle from-custom-purple/30 dark:from-custom-purple/40 from-0% via-custom-purple/20 dark:via-custom-purple/30 via-25% to-transparent to-70% 
-       h-[64rem] w-[32rem] md:h-[64rem] md:w-[48rem] blur-3xl absolute top-[-28rem] lg:top-[-32rem] right-[-12rem] lg:right-[0rem] z-0 "
+        className="rounded-t-full rounded-b-none bg-radial-circle from-custom-purple/30 dark:from-custom-purple/40 from-0% via-custom-purple/50 dark:via-custom-purple/30 via-25% to-transparent to-70% 
+              h-[16rem] w-[32rem] md:h-[24rem] md:w-[48rem] blur-3xl absolute bottom-0 left-[-12rem] lg:left-[0rem] z-0 "
       />
 
-      <div className="w-screen mt-32 flex h-8 justify-center lg:hidden z-10 container relative">
-        <SectionNameImage
-          lightImage="/assets/section_wordmark/about_h_dark.svg"
-          darkImage="/assets/section_wordmark/about_h.svg"
-          width={150}
-          height={100}
-        />
-      </div>
-
-      <div className="  grid grid-cols-12 px-8 gap-0 w-screen container z-10 relative">
-        <div className="sticky top-0 h-screen items-center w-14 lg:col-start-1 lg:col-end-2 hidden lg:flex">
-          <SectionNameImage
-            lightImage="/assets/section_wordmark/about_dark.svg"
-            darkImage="/assets/section_wordmark/about.svg"
-            width={75}
-            height={50}
-          />
-        </div>
-
-        <div
-          id="contact"
-          className="md:px-8  w-full col-start-1 lg:col-start-2 col-end-13 "
-        >
-          <div className="mt-24 lg:mt-48  flex flex-col gap-24 items-center md:px-4 lg:px-12 z-20 relative">
-            {/**Who Am I */}
-            <div className=" text-left flex flex-col">
-              <h3 className="text-xl md:text-2xl  lg:text-3xl font-medium">
-                Hi. I'm
-              </h3>
-
+      {/* Content */}
+      <div className="  px-8 gap-0 w-screen container z-10 relative">
+        <div id="contact" className="md:px-8  w-full ">
+          <div className=" flex flex-col gap-24 items-center md:px-4 lg:px-12 z-20 relative">
+            {/**Interested?*/}
+            <div className=" text-center flex flex-col items-center justify-center h-screen xl:max-h-[1280px] text-custom-dark-light/80 dark:text-custom-white-dark/80">
               <div
                 className="mt-2 font-bold text-5xl md:text-6xl  lg:text-7xl w-fit h-full-pb bg-clip-text
               text-transparent bg-gradient-flow bg-[length:200%_auto] animate-gradient "
               >
-                Jerard Pagalilauan
+                Interested?
               </div>
 
-              <p className="mt-4 text-lg md:text-xl  lg:text-xl max-w-[48rem] dark:text-custom-white-dark/80 text-custom-dark-light/80">
-                A curious and hardworking{" "}
-                <b>Bachelor of Science in Information Technology</b> graduate
-                with experience in developing web and desktop apps, who’s
-                dedicated in bringing creative and functional web designs to
-                life through programming.
-              </p>
+              <h3 className="text-xl md:text-2xl  lg:text-xl font-bold ">
+                Connect With Me
+              </h3>
 
-              <div className="mt-12 bg-gradient-flow bg-[length:200%_auto] animate-gradient w-1/2 sm:w-1/4 h-8 rounded-xl"></div>
+              <div className="flex gap-4 mt-4">
+                {ContactData.sort((a, b) => a.order - b.order).map((data) => (
+                  <a href={data.link} target="blank">
+                    <data.Icon className="h-6 w-full hover:text-custom-dark dark:hover:text-custom-white" />
+                  </a>
+                ))}
+              </div>
             </div>
           </div>
         </div>
