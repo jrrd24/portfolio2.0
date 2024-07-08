67c2b2bbe50f2240f76c6ab3bf0500a60acaaee9
jrrd24 <jerardpagalilauan@gmail.com>
2024-07-08 18:39:16 +0800

    :dizzy: Added Animation to Contact Links

--- app/data/ContactData.tsx
+++ app/data/ContactData.tsx
@@ -10,23 +10,38 @@ const ContactData = [
   {
     id: 1,
     Icon: FaFacebook,
+    name: "Facebook",
     link: "https://www.facebook.com/Jrrdprz/",
     order: 3,
   },
-  { id: 2, Icon: FaCommentSms, link: "tel:+639663672973", order: 2 },
+  {
+    id: 2,
+    Icon: FaCommentSms,
+    name: "Phone",
+    link: "tel:+639663672973",
+    order: 2,
+  },
   {
     id: 3,
     Icon: MdEmail,
+    name: "Email",
     link: "mailto:jerardpagalilauan@gmail.com",
     order: 1,
   },
   {
     id: 4,
     Icon: FaLinkedin,
+    name: "LinkedIn",
     link: "https://www.linkedin.com/in/jerard-pagalilauan-1571142a8/",
     order: 4,
   },
-  { id: 5, Icon: FaGithub, link: "https://github.com/jrrd24", order: 5 },
+  {
+    id: 5,
+    Icon: FaGithub,
+    name: "Github",
+    link: "https://github.com/jrrd24",
+    order: 5,
+  },
 ];
 
 export default ContactData;
